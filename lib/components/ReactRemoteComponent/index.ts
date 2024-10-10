2cc616e153117b3b85dcd01339e001a8dabfa3f7
szy <187384130@qq.com>
2024-10-11 00:50:36 +0800

    feat: react组件加载 && 类型声明

--- lib/components/ReactRemoteComponent/index.ts
+++ lib/components/ReactRemoteComponent/index.ts
@@ -1,3 +1,35 @@
-import ReactRemoteComponent from './ReactRemoteComponent'
+import React from "react";
+import { RemoteComponentProps } from './interface'
+
+class ReactRemoteComponent<T> extends React.Component<RemoteComponentProps<T>> {
+    constructor(props:RemoteComponentProps<T>) {
+        super(props);
+        this.state = {
+            Component: null,
+            ComponentVersion: null,
+            ComponentSourceUrl: null,
+            loading: true,
+            error: null,
+        }
+    }
+    componentDidMount(): void {
+        this.loadComponent();
+    }
+    getComponentSourceUrl = ({ name, version}) => {
+        
+    }
+
+    loadComponent = async () => {
+        const { name, version } = this.props;
+        try {
+            const { sourceUrl } = await this.getComponentSourceUrl({
+                name,
+                version
+            })
+        } catch (err) {
+            
+        }
+    }
+}
 
 export default ReactRemoteComponent;
---
+++ lib/components/ReactRemoteComponent/interface.ts
@@ -0,0 +1,30 @@
+import React from "react";
+interface RenderComponent {
+    loading: boolean;
+    error: any;
+    Component: React.ComponentType | null;
+}
+
+export interface RemoteComponentProps<T> {
+    /**
+    * 远程组件名
+    */
+    name: string;
+    /**
+    * 组件版本
+    */
+    version?: string;
+    /**
+    * 自定义渲染逻辑
+    */
+    render?: (props: RenderComponent) => React.ReactElement;
+    /**
+    * 渲染完成回调
+    */
+    onComponentRendered?: () => void;
+    /**
+    * 渲染失败回调
+    */  
+    onComponentRenderFailed?: () => void;
+
+}
